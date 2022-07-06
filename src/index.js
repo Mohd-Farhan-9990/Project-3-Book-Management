6b233d640bf9c8d5afc7dd66fb87bdc5e873bada
Mohd-Farhan-9990 <mohdfarhan9639@gmail.com>
2022-07-06 12:10:42 +0530

    Updated

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const boydParser = require('body-parser')
+const route  = require("./routes/route")
+const {default:mongoose} = require('mongoose')
+const bodyParser = require('body-parser')
+const app =express()
+
+app.use(bodyParser.json())
+
+const url = "mongodb+srv://MohdFarhan9990:LxP62162nUtlmSsp@cluster0.1zhmxfg.mongodb.net/group43Database"
+
+mongoose.connect(url,{useNewUrlParser:true})
+.then(()=>console.log("MongoDB Is Connected"))
+.catch(err=>console.log(err.message))
+
+app.use('/',route)
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("Express app is running on PORT "+(process.env.PORT||3000))
+})
